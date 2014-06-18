ade898d281e71e45ec7435e0f202437e3cede5c5
Geoffrey Liu <main@g-liu.com>
2014-06-18 00:27:40 -0700

    more conversion functions working
    support for decimal percentages in RGB (e.g. 68.2%) still a WIP.

--- colorparser.js
+++ colorparser.js
@@ -1,25 +1,26 @@
-String.prototype.toRGB = function() {
-	var _rgb = this.toRGBArray();
-	var rgb = {
+String.prototype.toRGB = function(usePercents) {
+	var _rgb = this.toRGBArray(usePercents);
+	return rgb = {
 		"r": _rgb[0],
 		"g": _rgb[1],
 		"b": _rgb[2]
 	};
-
-	return rgb;
 }
 
-String.prototype.toRGBArray = function() {
-	var type = getType(this);
+String.prototype.toRGBArray = function(usePercents) {
+	var type = getType.call(this);
 	var rgbArray = [-1, -1, -1];
 	switch(type) {
 		case 1: // RGB
 			var rgb = this.match(/\d{1,3}%?/gi);
-			var usePercent = (rgb[0][rgb[0].length - 1] === "%")
+			var asPercent = (rgb[0][rgb[0].length - 1] === "%")
 			for(var i = 0; i < rgb.length; i++) {
 				rgb[i] = parseInt(rgb[i], 10);
-				if(usePercent) {
+				if(asPercent && !usePercents) {
 					rgb[i] = Math.round(rgb[i] * 2.55);
+				}
+				else if(!asPercent && !!usePercents) {
+					rgb[i] /= 2.55;
 				}
 			}
 			rgbArray = rgb.slice(0, 3);
@@ -43,8 +44,10 @@ String.prototype.toRGBArray = function() {
 				default: rgb = [0, 0, 0]; break;
 			}
 			for(var i = 0; i < rgb.length; i++) {
-				rgb[i] += hsl[2] - 0.5 * chroma;
-				rgbArray[i] = Math.round(rgb[i] * 255);
+				rgbArray[i] += hsl[2] - 0.5 * chroma;
+				if(!usePercents) {
+					rgbArray[i] = Math.round(rgbArray[i] * 255);
+				}
 			}
 			break;
 		case 0: // HEX
@@ -54,112 +57,44 @@ String.prototype.toRGBArray = function() {
 				that = that[0] + that[0] + that[1] + that[1] + that[2] + that[2];
 			}
 			var bigint = parseInt(that, 16);
-			rgbArray[0] = (bigint >> 16) & 255;
-			rgbArray[1] = (bigint >> 8) & 255;
-			rgbArray[2] = bigint & 255;
+			rgbArray[0] = ((bigint >> 16) & 255) / (!!usePercents ? 255 : 1);
+			rgbArray[1] = ((bigint >> 8) & 255) / (!!usePercents ? 255 : 1);
+			rgbArray[2] = (bigint & 255) / (!!usePercents ? 255 : 1);
 			break;
 	}
 
 	return rgbArray;
+}
 
-	/**
-	 * Retrieve the color unit being used
-	 * @param {String} color
-	 * @return ...
-	 */
-	function getType(color) {
-		if(isHex()) {
-			return 0;
+String.prototype.toRGBString = function(usePercents) {
+	if(!usePercents) {
+		return "rgb(" + this.toRGBArray().join(",") + ")";
+	}
+	else {
+		var values = this.toRGBArray();
+		for(var i = 0; i < values.length; i++) {
+			values *= 100;
+			values += "%";
 		}
-		else if(isRGB()) {
-			return 1;
-		}
-		else if(isHSL()) {
-			return 2;
-		}
-		else {
-			throw("Not a color: '" + color + "' is not a color.");
-		}
-
-		function isHex() {
-			return color.match(/^#?[A-F0-9]{3}([A-F0-9]{3})?$/i);
-		}
-
-		function isRGB() {
-			// preliminary "loose" regex match
-			if(color.match(/^rgb\(\s*(\d{1,3}%?\s*,\s*){2}(\d{1,3}%?)\s*\)$/i)) {
-				var nums = color.match(/[0-9]{1,3}%?/gi);
-				if(!nums || !nums.length || nums.length != 3) {
-					return false;
-				}
-
-				// check format of each
-				var usePercent = (nums[0][nums[0].length - 1] === "%");
-				for(var i = 0; i < 3; i++) {
-					var numInt = parseInt(nums[i], 10);
-					if(usePercent && (nums[i][nums[i].length - 1] !== "%" || !(numInt >= 0 && numInt <= 100))) {
-						return false;
-					}
-					else if(numInt < 0 || numInt > 255) {
-						return false;
-					}
-				}
-				return true;
-			}
-
-			return false;
-		}
-
-		function isHSL() {
-			// preliminary "loose" regex match
-			if(color.match(/^hsl\(\s*\d{1,3}\s*,\s*\d{1,3}%\s*,\s*\d{1,3}%\s*\)$/i)) {
-				var nums = color.match(/\d{1,3}%?/gi);
-				if(!nums || !nums.length || nums.length !== 3) {
-					return false;
-				}
-
-				// check first num separately
-				if(nums[0][nums[0].length - 1] === "%" || !(nums[0] >= 0 && nums[0] <= 360)) {
-					return false;
-				}
-				for(var i = 1; i < 3; i++) {
-					var numInt = parseInt(nums[i], 10);
-					if(nums[i][nums[i].length - 1] !== "%" || !(numInt >= 0 && numInt <= 100)) {
-						return false;
-					}
-				}
-				return true;
-			}
-
-			return false;
-		}
+		return "rgb(" + values.join(",") + ")";
 	}
 }
 
-String.prototype.toRGBString = function() {
-	var rgbStringStart = "rgb(";
-	var rgbStringEnd = ")";
-
-	return rgbStringStart + this.toRGBArray().join(",") + rgbStringEnd;
-}
-
 String.prototype.toHex = function() {
-	var hex = {
-		"r": "00",
-		"g": "00",
-		"b": "00"
+	var _h = this.toHexArray();
+	return hex = {
+		"r": _h[0],
+		"g": _h[1],
+		"b": _h[2]
 	};
-
-	// TODO: Implement conversion
-
-	return hex;
-
 }
 
 String.prototype.toHexArray = function() {
-	var hexArray = ["00", "00", "00"];
-
-	// TODO: Implement conversion
+	var rgb = this.toRGBArray();
+	var hexArray = [];
+	for(var i = 0; i < rgb.length; i++) {
+		hexArray[i] = rgb[i].toString(16);
+	}
 
 	return hexArray;
 }
@@ -169,32 +104,117 @@ String.prototype.toHexArray = function() {
  */
 String.prototype.toHexString = function(prefix) {
 	var pfx = prefix ? "#" : "";
-	return pfx + this.toHexArray().join();
+	return pfx + this.toHexArray().join("");
 }
 
 String.prototype.toHSL = function() {
-	var hsl = {
-		"h": 0,
-		"s": 0,
-		"l": 0
+	var _hsl = this.toHSLArray();
+	return hsl = {
+		"h": _hsl[0],
+		"s": _hsl[1],
+		"l": _hsl[2]
 	};
-
-	// TODO: Implement conversion
-
-	return hsl;
 }
 
+/**
+ * http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+ */
 String.prototype.toHSLArray = function() {
-	var hslArray = [0, 0, 0];
+	var rgb = this.toRGBArray(true);
+	var r = rgb[0], g = rgb[1], b = rgb[2];
 
-	// TODO: Implement conversion
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+	
+	if(max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
 
-	return hslArray;
+    return [h, s, l];
 }
 
 String.prototype.toHSLString = function() {
-	var hslStringStart = "hsl(";
-	var hslStringEnd = ")";
+	return "hsl(" + this.toHSLArray().join(",") + ")";
+}
 
-	return hslStringStart + this.toHSLArray().join(",") + hslStringEnd;
+/**
+ * Retrieve the color unit being used
+ * @param {String} color
+ * @return ...
+ */
+function getType() {
+	if(isHex.call(this)) {
+		return 0;
+	}
+	else if(isRGB.call(this)) {
+		return 1;
+	}
+	else if(isHSL.call(this)) {
+		return 2;
+	}
+	else {
+		throw("Not a color: '" + this + "' is not a color.");
+	}
+
+	function isHex() {
+		return this.match(/^#?[A-F0-9]{3}([A-F0-9]{3})?$/i);
+	}
+
+	function isRGB() {
+		// preliminary "loose" regex match
+		if(this.match(/^rgb\(\s*(\d{1,3}((\.\d+%)?|%)\s*,\s*){2}(\d{1,3}((\.\d+%)?|%))\s*\)$/i)) {
+			var nums = this.match(/[0-9]{1,3}((\.\d+%)?|%)/gi);
+			if(!nums || !nums.length || nums.length != 3) {
+				return false;
+			}
+
+			// check format of each
+			var usePercent = (nums[0][nums[0].length - 1] === "%");
+			for(var i = 0; i < 3; i++) {
+				var numInt = parseFloat(nums[i], 10);
+				if(usePercent && (nums[i][nums[i].length - 1] !== "%" || !(numInt >= 0 && numInt <= 100))) {
+					return false;
+				}
+				else if(numInt < 0 || numInt > 255) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		return false;
+	}
+
+	function isHSL() {
+		// preliminary "loose" regex match
+		if(this.match(/^hsl\(\s*\d{1,3}\s*,\s*\d{1,3}(\.\d+)?%\s*,\s*\d{1,3}(\.\d+)?%\s*\)$/i)) {
+			var nums = this.match(/\d{1,3}(\.\d+%)?/gi);
+			if(!nums || !nums.length || nums.length !== 3) {
+				return false;
+			}
+
+			// check first num separately
+			if(nums[0][nums[0].length - 1] === "%" || !(nums[0] >= 0 && nums[0] <= 360)) {
+				return false;
+			}
+			for(var i = 1; i < 3; i++) {
+				var numInt = parseFloat(nums[i], 10);
+				if(nums[i][nums[i].length - 1] !== "%" || !(numInt >= 0 && numInt <= 100)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		return false;
+	}
 }
